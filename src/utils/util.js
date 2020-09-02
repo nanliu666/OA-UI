830f4d37d3cfc35d5b73b2447ba22f6b565cf784
sutuo <sutuo1988@live.com>
2020-09-02 14:13:34 +0800

    feat: 将工具函数添加到工具库

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,4 @@
-export const getObjType = obj => {
+export const getObjType = (obj) => {
   var toString = Object.prototype.toString
   var map = {
     '[object Boolean]': 'boolean',
@@ -20,7 +20,7 @@ export const getObjType = obj => {
 /**
  * 对象深拷贝
  */
-export const deepClone = data => {
+export const deepClone = (data) => {
   var type = getObjType(data)
   var obj
   if (type === 'array') {
@@ -61,4 +61,37 @@ export const get = (obj, path, defaultValue) => {
     }
   }
   return current
+}
+
+/**
+ * 对象数组扁平化
+ * @param {{[props:string]:any, children: any[]}[]} target 要进行扁平化的对象数组
+ * @param {String} children 对象的children属性
+ * @returns {Iterable<any>} 扁平化后的数组
+ */
+export function* flatten(target, children = 'children') {
+  for (const item of target) {
+    if (Array.isArray(item[children])) {
+      yield* [item, ...flatten(item[children])]
+    } else {
+      yield item
+    }
+  }
+}
+
+/**
+ * 省略对象的指定属性，返回一个浅拷贝的对象
+ * @param {object} target 目标对象
+ * @param {String|String[]} props 要省略的属性
+ * @returns {object} 返回对象
+ */
+export function omit(target, props) {
+  const result = {}
+  props = Array.isArray(props) ? props : [props]
+  for (const key in target) {
+    if (!props.includes(key)) {
+      result[key] = target[key]
+    }
+  }
+  return result
 }
