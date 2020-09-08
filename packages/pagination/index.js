bc55fed5ce997dcc99eb661bbf5052ea70e40ce7
xiangrun <xiangrun.kong@epro.com.cn>
2020-09-08 15:48:46 +0800

    fix: pagination修改样式为saber内的样式

--- packages/pagination/index.js
+++ packages/pagination/index.js
@@ -1,4 +1,5 @@
 import { Pagination } from 'element-ui'
+import './pagination.scss'
 Pagination.mgName = 'MgPagination'
 Pagination.install = function(Vue) {
     Vue.component(Pagination.mgName, Pagination)
