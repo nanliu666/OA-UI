b7574a619165d54ba25a645417a13114f82cec7f
xiangrun <xiangrun.kong@epro.com.cn>
2020-09-16 14:11:30 +0800

    fix: 更改tabs的复用形式（copy+修改name ==> import+修改mgName ）

--- packages/tabs/index.js
+++ packages/tabs/index.js
@@ -1,8 +1,8 @@
-import Tabs from './src/tabs'
-
-/* istanbul ignore next */
+import { Tabs } from 'element-ui'
+Tabs.mgName = 'MgTabs'
 Tabs.install = function(Vue) {
   Vue.component(Tabs.mgName, Tabs)
 }
 
 export default Tabs
+
