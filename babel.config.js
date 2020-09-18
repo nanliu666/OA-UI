363ff419ad16f007d20f2715389b3f7d94854323
liukangqi <liukangqi56@hotmail.com>
2020-09-18 18:12:17 +0800

    build: 直接引入antd到组件库

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'antd',
-        style: true // or 'css'
-      }
-    ]
-  ]
+  presets: ['@vue/cli-plugin-babel/preset']
 }
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,28 @@
 const path = require('path')
-const fs = require('fs')
-const lessToJS = require('less-vars-to-js')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './src/styles/antd-vars.less'), 'utf8')
-)
+
 module.exports = {
   chainWebpack: (config) => {
     config.resolve.alias.set('@', resolve('src'))
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial' // only package third parties that are initially dependent
+        }
+      }
+    })
   },
   css: {
     loaderOptions: {
       less: {
         lessOptions: {
-          javascriptEnabled: true, // 加载LESS 需要把JS设置一下
-          modifyVars: themeVariables
+          javascriptEnabled: true // 加载LESS 需要把JS设置一下
         }
       }
     }
